c1acfc50d30782a2331cc970b8c97d2185946dc0
chooselinyuliang <74659264+chooselinyuliang@users.noreply.github.com>
2020-11-25 21:44:01 +0800

    键盘敲击

---
+++ 112501.键盘敲击.App.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public App() {
+
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    textArea_B.append("你按了上键！\n");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        textArea_A.setText("点击键盘上键输出文字");
+        textArea_A.setBounds(100,100,256,128);
+        textArea_B.setBounds(100,300,256,128);
+        textArea_A.setBackground(Color.pink);
+        textArea_B.setBackground(Color.yellow);
+        myPanel.add(textArea_A);
+        myPanel.add(textArea_B);
+        JFrame frame = new JFrame("键盘点击事件");
+        myPanel.setLayout(null);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
